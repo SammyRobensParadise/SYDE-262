346ff502e1474ac3c873fb590584b1d94be2f60b
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-10-03 22:43:34 -0400

    finished the q

--- assign2_Qa.m
+++ assign2_Qa.m
@@ -23,12 +23,22 @@ disp(producer_surplus);
 disp('b)')
 % shift with $1 tax on seller or supply shift left
 supply_with_tax=q-14;
+seller_deadweight_loss=0.5*(5.5-4.5)*0.5;
+disp('Seller deadweight loss');
+disp(seller_deadweight_loss);
+
+disp('c)');
+% shift with $1 tax on supplier or demand shift left
+demand_with_tax=-q+24;
+supplier_deadweight_loss=0.5*(20-19.5)*(5-4);
+disp('Supplier deadweight loss')
+disp(supplier_deadweight_loss);
 
 % plot initial graph
-plot(q,supply,q,demand,q,supply_with_tax);
+plot(q,supply,q,demand,q,supply_with_tax,q,demand_with_tax);
 title('Demand vs. Supply curve for a widget');
 ylabel('Price');
 xlabel('Quantity');
-legend({'Supply','Demand','Supply with tax'},'Location','southwest')
+legend({'Supply','Demand','Seller with tax','Supplier with tax'},'Location','southwest')
 
 
