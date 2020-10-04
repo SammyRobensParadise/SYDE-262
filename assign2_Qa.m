3b983b70b7d31d1b3874de9b82e10c86c5fab471
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-10-03 21:12:44 -0400

    completed 1a)

--- assign2_Qa.m
+++ assign2_Qa.m
@@ -1,20 +1,30 @@
-x1=20;
-x2=21;
-y1=5;
-y2=4;
-q=0:0.01:1;
-demand_slope=(y2-y1)/(x2-x1);
-demand_slope_intercept=y1-demand_slope*x1;
-demand_slope_func=demand_slope*q+demand_slope_intercept;
-supply_x1=4;
-supply_x2=20;
-supply_y1=19;
-supply_y2=5;
-supply_slope=(supply_y2-supply_y1)/(supply_x2-supply_x1);
-supply_slope_intercept=supply_y1-supply_slope*supply_x1;
-supply_slope_func=supply_slope*q+supply_slope_intercept;
-disp(supply_slope);
-disp(demand_slope);
-plot(demand_slope_func);
-hold on;
-plot(supply_slope_func);
+% setting up the quant range
+q=0:40;
+
+% get demand supply lines initially 
+supply=q-15;
+demand=-1*q+25;
+
+% get intersection of demand and supply curve
+intersection=find(supply==demand)-1;
+intersection_y=supply(20)+1;
+
+disp('a)');
+% calculate consumer surplus
+consumer_surplus=0.5*20*(25-5);
+disp('Consumer Surplus');
+disp(consumer_surplus);
+
+% calculate producer surplus
+producer_surplus=0.5*20*5;
+disp('Producer Surplus');
+disp(producer_surplus);
+
+% plot initial graph
+plot(q,supply,q,demand);
+title('Demand vs. Supply curve for a widget');
+ylabel('Price');
+xlabel('Quantity');
+legend({'Supply','Demand'},'Location','southwest')
+
+
